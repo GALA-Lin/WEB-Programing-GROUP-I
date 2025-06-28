778b80a74b6bce18dd5ba1088785f161343adb82
郑智（ZhiZheng） <zz530346@gmail.com>
2025-06-28 13:01:05 +0800

    Update AdminActivityController.java

--- volunteer-platform/src/main/java/com/student/webproject/controller/AdminActivityController.java
+++ volunteer-platform/src/main/java/com/student/webproject/controller/AdminActivityController.java
@@ -1,8 +1,9 @@
 package com.student.webproject.controller;
 
+import com.student.webproject.dto.ActivityCreateDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AdminActivityController {
     @PostMapping
-    public String createActivity() {
-        return "成功接收到发布新活动的请求！";
+    public String createActivity(@RequestBody ActivityCreateDTO activityData) {
+        // 为了验证我们是否成功收到了数据，我们把它打印出来
+        System.out.println("成功收到前端发来的数据：" + activityData.toString());
+        // 返回一句话，证明数据已经收到
+        return "成功接收到活动标题：" + activityData.getTitle();
     }
 }
