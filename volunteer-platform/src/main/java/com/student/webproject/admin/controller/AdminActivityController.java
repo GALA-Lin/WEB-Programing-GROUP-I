bb21cbd33ab795cfe19046372b6234ff199fe354
郑智（ZhiZheng） <zz530346@gmail.com>
2025-06-29 10:27:18 +0800

    Revert "Update AdminActivityController.java"
    This reverts commit acfe687118e1cc65daa0140c77a87fce185f2154.

--- volunteer-platform/src/main/java/com/student/webproject/admin/controller/AdminActivityController.java
+++ volunteer-platform/src/main/java/com/student/webproject/admin/controller/AdminActivityController.java
@@ -3,7 +3,10 @@ package com.student.webproject.admin.controller;
 import com.student.webproject.admin.dto.ActivityCreateDTO;
 import com.student.webproject.admin.service.ActivityAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin/activities")
@@ -21,20 +24,5 @@ public class AdminActivityController {
     @PostMapping
     public String createActivity(@RequestBody ActivityCreateDTO activityData) {
         return activityAdminService.createActivity(activityData);
-    }
-    /**
-     * 新增的“更新”接口
-     * @PutMapping("/{id}"): 告诉 Spring Boot, 这个方法处理 PUT 类型的、
-     * 并且路径末尾带有ID的请求。例如: PUT http://localhost:8080/api/admin/activities/1
-     * @PathVariable Long id: 告诉 Spring Boot, "请把路径中 {id} 部分的值，取出来赋给 long id 这个变量。"
-     * @RequestBody ActivityCreateDTO dto: 和之前一样，接收请求体中的 JSON 数据。
-     */
-    @PutMapping("/{id}")
-    public String updateActivity(@PathVariable Long id, @RequestBody ActivityCreateDTO dto) {
-        System.out.println("Controller 收到了更新请求，要更新的活动ID是: " + id);
-        System.out.println("更新后的数据是: " + dto.toString());
-
-        // TODO: 我们稍后会在这里调用 Service 的更新方法
-        return "成功接收到更新请求！";
     }
 }
