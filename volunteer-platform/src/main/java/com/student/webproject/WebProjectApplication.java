bebc74694b3d0566b4ae03e6e5406f892272bd6b
Zivicii <1948607907@qq.com>
2025-06-30 12:56:47 +0800

    改完bug

--- volunteer-platform/src/main/java/com/student/webproject/WebProjectApplication.java
+++ volunteer-platform/src/main/java/com/student/webproject/WebProjectApplication.java
@@ -1,13 +1,13 @@
 package com.student.webproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.student.webproject") // <-- 必须有这一行，它告诉框架去哪里找 Mapper 接口
 public class WebProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebProjectApplication.class, args);
     }
-
 }
