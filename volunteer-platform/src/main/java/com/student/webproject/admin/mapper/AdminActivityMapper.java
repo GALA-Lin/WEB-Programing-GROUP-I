2b8645573d706d3fb56da4b7f11b296c0454cef5
Linsen HU <1563883475@qq.com>
2025-07-03 01:48:05 +0800

    解决冲突活动实体类

--- volunteer-platform/src/main/java/com/student/webproject/admin/mapper/AdminActivityMapper.java
+++ volunteer-platform/src/main/java/com/student/webproject/admin/mapper/AdminActivityMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 活动表的 Mapper 接口 (数据库操作手)
- * @Mapper 注解告诉 Spring Boot 和 MyBatis-Plus: "这是一个操作数据库的 Mapper 接口，请帮我实现它。"
+ * //@Mapper 注解告诉 Spring Boot 和 MyBatis-Plus: "这是一个操作数据库的 Mapper 接口，请帮我实现它。"
  */
 @Mapper
 public interface AdminActivityMapper extends BaseMapper<Activity> {
@@ -15,5 +15,4 @@ public interface AdminActivityMapper extends BaseMapper<Activity> {
     // 通过继承 BaseMapper<Activity>，我们就自动拥有了对 Activity 实体的
     // 增、删、改、查等一系列常用方法，比如 insert(), deleteById(), updateById(), selectById() 等。
     // 我们暂时不需要在这里手写任何方法。
-
 }
