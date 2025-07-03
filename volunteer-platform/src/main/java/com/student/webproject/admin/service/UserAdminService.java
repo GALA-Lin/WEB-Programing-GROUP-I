0057afaaea9edb5ecbcadfb63e3393b6a1aecdfb
郑智（ZhiZheng） <zz530346@gmail.com>
2025-07-04 01:06:19 +0800

    修复后端登录权限过于宽泛/活动修改异常/用户权限分层不明显的问题

--- volunteer-platform/src/main/java/com/student/webproject/admin/service/UserAdminService.java
+++ volunteer-platform/src/main/java/com/student/webproject/admin/service/UserAdminService.java
@@ -4,13 +4,20 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.student.webproject.admin.dto.UserCreateDTO;
 import com.student.webproject.admin.dto.UserPasswordUpdateDTO;
 import com.student.webproject.admin.dto.UserUpdateDTO;
-import com.student.webproject.user.Entity.User;
 import com.student.webproject.common.response.Result;
+import com.student.webproject.user.Entity.User;
+
+import java.security.Principal;
 
 public interface UserAdminService {
+
     Result<IPage<User>> listUsers(Long page, Long pageSize);
-    Result<User> createUser(UserCreateDTO dto);
-    Result<User> updateUser(Long id, UserUpdateDTO dto);
+
+    Result<User> createUser(Principal principal, UserCreateDTO dto);
+
+    Result<User> updateUser(Principal principal, Long id, UserUpdateDTO dto);
+
     Result<Void> deleteUser(Long id);
+
     Result<Void> updateUserPassword(Long id, UserPasswordUpdateDTO dto);
 }
